87f049d660bb4e8b7387bfa50dd27e4911ba0ca4
vLabayen <victor.mendi3216@gmail.com>
2020-07-27 23:35:37 +0200

    Movement v2 & playerStats

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class PlayerMovement : MonoBehaviour
 {
+  public PlayerStats stats;
 
-  public float movementSpeed;
-  public float jumpForce;
-  public float gravityScale;
   private Rigidbody2D rb;
   private bool isGrounded = true;
 
   void Start() {
     this.rb = this.GetComponent<Rigidbody2D>();
     this.rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-    this.rb.gravityScale = this.gravityScale;
+    this.rb.gravityScale = this.stats.gravityScale;
   }
 
   void FixedUpdate() {
     float x = this.getInputX();
-    this.transform.Translate(Vector3.right * x * this.movementSpeed * Time.fixedDeltaTime);
+    this.transform.Translate(Vector3.right * x * this.stats.movementSpeed * Time.fixedDeltaTime);
 
     if (this.isGrounded && this.getInputY() > 0) {
-      this.rb.AddForce(Vector2.up * this.jumpForce, ForceMode2D.Impulse);
+      this.rb.velocity = new Vector2(0f, 0f);
+      this.rb.AddForce(Vector2.up * this.stats.jumpForce, ForceMode2D.Impulse);
       this.isGrounded = false;
     }
   }
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player", menuName = "Stats/Player")]
+public class PlayerStats : ScriptableObject {
+  public float movementSpeed;
+  public float jumpForce;
+  public float gravityScale;
+}
