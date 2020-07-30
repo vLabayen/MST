3a90a24e032b37b1d64d46182e2b1b4eb438ac95
jadanero <58793838+jadanero@users.noreply.github.com>
2020-07-30 21:52:11 +0200

    menu de mierda  enter the commit message for your changes. Lines starting

---
+++ Assets/Scripts/System/ControlMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ControlMenu : MonoBehaviour
+{
+    public void Menu(string level) {
+        SceneManager.LoadScene(level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
