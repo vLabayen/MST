5e3218671e5543c2950018e8f333b33a2edb872f
jadanero <58793838+jadanero@users.noreply.github.com>
2020-07-30 23:14:01 +0200

    mejor menu

--- Assets/Scripts/System/ControlMenu.cs
+++ Assets/Scripts/System/ControlMenu.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 
 public class ControlMenu : MonoBehaviour
 {
+    public static ControlMenu singleton { get; private set; }
+
     public void Menu(string level) {
         SceneManager.LoadScene(level);
+    }
+
+    public void Awake()
+    {
+        if (ControlMenu.singleton == null)
+        {
+            ControlMenu.singleton = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else Destroy(this.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
     }
 }
